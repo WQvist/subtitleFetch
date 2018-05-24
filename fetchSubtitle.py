dd1aedec00b02a54c652750c41048e97f6b882f6
William Öqvist <williamoqvist@hotmail.com>
2018-05-24 18:30:58 +0200

    Updated to automatically do everything

--- fetchSubtitle.py
+++ fetchSubtitle.py
@@ -1,6 +1,20 @@
-import subscene, requests, zipfile, io
+# 1. In a folder, enter "shell:sendto" in address bar
+# 2. There, create a .cmd-file with:
+#		@echo off
+#		cls
+#		python C:\Your\File\uploadscript.py %1
+# 3. Voila! You can right click and send to python script
+
+import subscene, requests, zipfile, io, sys
+
+# Fetch string between last \ and .
+stringToCheck = sys.argv[1]
+placeOfLastBackslash = stringToCheck.rfind("\\", 0, len(stringToCheck)-3)
+placeOfDot = stringToCheck.rfind(".")
+
+# Find subtitles for title
+filmReq = stringToCheck[placeOfLastBackslash+1:placeOfDot]
 listOfAllEngSubtitles = []
-filmReq = input("Enter title: ")
 film = subscene.search(filmReq)
 
 # Fetch all english subtitles
@@ -8,14 +22,8 @@ for subtitle in film.subtitles:
 	if subtitle.language == "English":
 		listOfAllEngSubtitles.append(subtitle)
 
-# Print all to choose
-count = 1		
-for subtitle in listOfAllEngSubtitles:
-	print(count, subtitle.title)
-	count+=1
-
-indexOfDownload = input("Enter number to download: ")
-
-r = requests.get(listOfAllEngSubtitles[int(indexOfDownload)-1].zipped_url)
+r = requests.get(listOfAllEngSubtitles[0].zipped_url)
 z = zipfile.ZipFile(io.BytesIO(r.content))
-z.extractall("F:/")
+
+# Save in folder in which script was called from
+z.extractall(stringToCheck[:placeOfLastBackslash+1])
