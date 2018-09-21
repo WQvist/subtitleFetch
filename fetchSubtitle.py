0de8c982cbdafed9c7464b99181a9a29db1643ac
William Öqvist <williamoqvist@hotmail.com>
2018-09-21 21:47:36 +0200

    Variable clarification and added renaming srt-file

--- fetchSubtitle.py
+++ fetchSubtitle.py
@@ -5,7 +5,7 @@
 #		python C:\Your\File\uploadscript.py %1
 # 3. Voila! You can right click and send to python script
 
-import subscene, requests, zipfile, io, sys
+import subscene, requests, zipfile, io, sys, os, glob
 
 # Fetch string between last \ and .
 inputParameter = sys.argv[1]
@@ -13,17 +13,23 @@ placeOfLastBackslash = inputParameter.rfind("\\")
 placeOfDot = inputParameter.rfind(".")
 
 # Find subtitles for title
-filmReq = inputParameter[placeOfLastBackslash+1:placeOfDot]
+filmTitle = inputParameter[placeOfLastBackslash+1:placeOfDot]
 listOfAllEngSubtitles = []
-film = subscene.search(filmReq)
+film = subscene.search(filmTitle)
 
 # Fetch all english subtitles
 for subtitle in film.subtitles:
 	if subtitle.language == "English":
 		listOfAllEngSubtitles.append(subtitle)
 
+# Grab the first subtitle
 r = requests.get(listOfAllEngSubtitles[0].zipped_url)
 z = zipfile.ZipFile(io.BytesIO(r.content))
 
 # Save in folder in which script was called from
 z.extractall(inputParameter[:placeOfLastBackslash+1])
+
+# Rename (This requires a single srt-file in directory)
+originalName = glob.glob('*.srt')[0]
+placeOfLastBackslash = originalName[0].rfind('\\')
+os.rename(originalName,originalName[:placeOfLastBackslash+1] + filmTitle + '.srt')
